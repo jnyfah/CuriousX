b0ea12ad8026e5bfd0908175381935d774d21e9f
Jennifer Chukwu <jnyfaah@gmail.com>
2022-08-17 12:18:02 +0000

    lexerEquality

--- tests/lexerTest.cpp
+++ tests/lexerTest.cpp
@@ -1,1 +1,10 @@
 #include "gtest/gtest.h"
+
+#include "lexer.hpp"
+
+void LexerEquality(std::vector<LexerToken> actual, std::vector<LexerToken> expected){
+    EXPECT_EQ(actual.size(), expected.size());
+    for(int i = 0; i < actual.size(); i++){
+        EXPECT_EQ(actual[i] , expected[i]);
+    }
+}
