f5b29eb6b9d52f2e2c286ef9f78f561a3f45bc1f
jnyfah <jnyfaah@gmail.com>
2024-09-12 19:41:48 +0100

    clang format

--- Utils/CompilerOutputParser.hpp
+++ Utils/CompilerOutputParser.hpp
@@ -4,47 +4,54 @@
 #include <nlohmann/json.hpp>
 #include <sstream>
 
-
-
 using json = nlohmann::json;
 
 class CompilerOutputParser
 {
   private:
-    static std::string formatValue(const std::string &value)
+    static std::string formatValue(const std::string& value)
     {
-        if (value.empty()) return "<empty>";
-        if (value == "\n") return "\\n";
-        if (value == "\t") return "\\t";
+        if (value.empty())
+            return "<empty>";
+        if (value == "\n")
+            return "\\n";
+        if (value == "\t")
+            return "\\t";
         return "[" + value + "]";
     }
 
-
   public:
-    static std::string readInputFile(const std::string &filePath)
+    static std::string readInputFile(const std::string& filePath)
     {
         std::ifstream inputFile(filePath);
-        if (!inputFile.is_open()) { throw std::runtime_error("Unable to open input file: " + filePath); }
+        if (!inputFile.is_open())
+        {
+            throw std::runtime_error("Unable to open input file: " + filePath);
+        }
         std::ostringstream sstr;
         sstr << inputFile.rdbuf();
         return sstr.str();
     }
 
-    static void formatTokens(const std::string &jsonString, const std::string &outputFile)
+    static void formatTokens(const std::string& jsonString, const std::string& outputFile)
     {
         nlohmann::json j = nlohmann::json::parse(jsonString);
         std::ofstream outFile(outputFile);
-        if (!outFile.is_open()) { throw std::runtime_error("Unable to open output file: " + outputFile); }
+        if (!outFile.is_open())
+        {
+            throw std::runtime_error("Unable to open output file: " + outputFile);
+        }
 
         outFile << "==== Lexer Output ====\n\n";
         outFile << std::left << std::setw(20) << "Token" << std::setw(20) << "Position"
                 << "Value\n";
         outFile << std::string(70, '-') << "\n";
 
-        for (const auto &token : j["lexer"])
+        for (const auto& token : j["lexer"])
         {
-            outFile << std::left << std::setw(20) << token["type"].get<std::string>() << std::setw(20) << std::setw(20)
-                    << token["location"].get<std::string>() << formatValue(token["value"].get<std::string>()) << "\n";
+            outFile << std::left << std::setw(20) << token["type"].get<std::string>()
+                    << std::setw(20) << std::setw(20) << token["location"].get<std::string>()
+                    << formatValue(token["value"].get<std::string>()) << "\n";
         }
 
         outFile.close();
--- Utils/Error.hpp
+++ Utils/Error.hpp
@@ -1,25 +1,21 @@
 #pragma once
+#include "SourceLocation.hpp"
 #include <string>
 #include <string_view>
 #include <utility>
-#include "SourceLocation.hpp"
 
 class Error : public std::exception
 {
-public:
-    explicit Error(std::string_view message = "An unknown error occurred.")
-        : message(message)
-    {}
-   
+  public:
+    explicit Error(std::string_view message = "An unknown error occurred.") : message(message) {}
+
     Error(std::string_view message, const SourceLocation& location)
         : message(message.data() + location.toString())
-    {}
-
-    const char* what() const noexcept override
     {
-        return message.c_str();
     }
 
-private:
+    const char* what() const noexcept override { return message.c_str(); }
+
+  private:
     std::string message;
 };
